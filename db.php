039bfacf0588a0e2912b1fe1b5f63b21d14d4b47
Estuardo <estuarjosm@outlook.com>
2020-09-11 16:11:17 -0600

    Prueba para test

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php 
+	session_start();
+	$conn =mysqli_connect(
+		'localhost:33065',
+		'root',
+		'',
+		'php_crud2'
+	);	
+?>
---
+++ include/nav.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-dark bg-dark">
+	<div class="container">
+		<a class="navbar-dark" href="index.php">PHP CRUD</a>	
+	</div>
+</nav>
